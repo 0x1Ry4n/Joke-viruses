baa70b5fc18b50ef5b7786468834fda49ebea5e5
Test <kingoftheglory2019@gmail.com>
2021-08-29 01:03:31 -0300

    simple aslr example

--- aslr.c
+++ aslr.c
@@ -6,3 +6,6 @@ int main(void)
 	printf("$esp = %#020x\n", i);
 	return 0;
 }
+
+//enable ASLR on linux: echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
+//disable ASLR on linux: echo 2 | sudo tee /proc/sys/kernel/randomize_va_space
